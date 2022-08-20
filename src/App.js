283afe3cb052efe03f901d4e57ca386b901e07dc
KCampbell22 <ksoup12@gmail.com>
2022-08-20 16:32:09 -0500

    -initializing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Home from "./Home";
+import Movie from "./Movie";
 
-function App() {
+import "./App.css";
+
+const NotFound = () => {
+  return <h2>404 Not Found</h2>;
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Movie Finder
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/movie/:id" element={<Movie />} />
+
+        <Route element={NotFound} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
